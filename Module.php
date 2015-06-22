efd523a7184a367749c6a78905cf1c096ca47d15
Андрей <@hotline>
2015-06-22 16:11:34 +0600

    Module

---
+++ Module.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @link https://github.com/borodulin/yii2-oauth2-server
+ * @copyright Copyright (c) 2015 Andrey Borodulin
+ * @license https://github.com/borodulin/yii2-oauth2-server/blob/master/LICENSE
+ */
+
+namespace conquer\oauth2;
+
+use conquer\oauth2\console\Oauth2Controller;
+/**
+ * @author Andrey Borodulin
+ */
+class Module extends \yii\base\Module implements \yii\base\BootstrapInterface
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app)
+    {
+        if ($app instanceof \yii\web\Application) {
+            $app->getUrlManager()->addRules([
+                    $this->id => $this->id . '/default/index',
+                    $this->id . '/<id:\w+>' => $this->id . '/default/view',
+                    $this->id . '/<controller:[\w\-]+>/<action:[\w\-]+>' => $this->id . '/<controller>/<action>',
+            ], false);
+        } elseif ($app instanceof \yii\console\Application) {
+            $app->controllerMap[$this->id] = [
+                    'class' => Oauth2Controller::className(),
+            ];
+        }
+    }
+
+}
---
+++ console/Oauth2Controller.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @link https://github.com/borodulin/yii2-oauth2-server
+ * @copyright Copyright (c) 2015 Andrey Borodulin
+ * @license https://github.com/borodulin/yii2-oauth2-server/blob/master/LICENSE
+ */
+
+namespace conquer\oauth2\console;
+
+use yii\console\Controller;
+use conquer\oauth2\models\AuthorizationCode;
+use conquer\oauth2\models\RefreshToken;
+use conquer\oauth2\models\AccessToken;
+
+/**
+ * @author Andrey Borodulin
+ */
+class Oauth2Controller extends Controller
+{
+    public function actionIndex()
+    {
+        
+    }
+    
+    public function actionClear()
+    {
+        AuthorizationCode::deleteAll(['<', 'expires', time()]);
+        RefreshToken::deleteAll(['<', 'expires', time()]);
+        AccessToken::deleteAll(['<', 'expires', time()]);
+    }
+}
