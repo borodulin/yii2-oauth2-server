0b514b8358ad5889dd049c5abb477f9c16d580f1
Андрей <@192.168.164.150>
2015-07-15 02:54:51 +0600

    fix Implicit

--- responsetypes/Implicit.php
+++ responsetypes/Implicit.php
@@ -7,23 +7,50 @@
 
 namespace conquer\oauth2\responsetypes;
 
-use conquer\oauth2\OAuth2Trait;
 /**
- * @property string $client_id
- * @property string $redirect_uri
- * @property string $scope
- *
+ * @link https://tools.ietf.org/html/rfc6749#section-4.2.1
  * @author Andrey Borodulin
  */
 class Implicit extends ResponseTypeAbstract
 {
-    public $tokeType = 'bearer';
+    const TOKEN_TYPE = 'bearer';
+    
+    /**
+     * Value MUST be set to "token"
+     * @var string
+     */
+    public $response_type;
+    /**
+     * The client identifier as described in Section 2.2.
+     * @link https://tools.ietf.org/html/rfc6749#section-2.2
+     * @var string
+     */
+    public $client_id;
+    /**
+     * As described in Section 3.1.2.
+     * @link https://tools.ietf.org/html/rfc6749#section-3.1.2
+     * @var string
+     */
+    public $redirect_uri;
+    /**
+     * The scope of the access request as described by Section 3.3.
+     * @link https://tools.ietf.org/html/rfc6749#section-3.3
+     * @var string
+     */
+    public $scope;
+    /**
+     * The parameter SHOULD be used for preventing cross-site request forgery as described in Section 10.12.
+     * @link https://tools.ietf.org/html/rfc6749#section-10.12
+     * @var string
+     */
+    public $state;
     
     public function rules()
     {
         return [
-            [['client_id'], 'required'],
-            [['client_id', 'state'], 'string', 'max' => 80],
+            [['client_id', 'response_type'], 'required'],
+            [['client_id'], 'string', 'max' => 80],
+            [['state'], 'string', 'max' => 255],
             [['redirect_uri'], 'url'],
             [['client_id'], 'validateClient_id'],
             [['redirect_uri'], 'validateRedirect_uri'],
@@ -35,25 +62,32 @@ class Implicit extends ResponseTypeAbstract
     public function getResponseData()
     {
         $accessToken = \conquer\oauth2\models\AccessToken::createAccessToken([
-                'client_id' => $this->client_id,
-                'user_id' => \Yii::$app->user->id,
-                'expires' => $this->accessTokenLifetime + time(),
-                'scope' => $this->scope,
+            'client_id' => $this->client_id,
+            'user_id' => \Yii::$app->user->id,
+            'expires' => $this->accessTokenLifetime + time(),
+            'scope' => $this->scope,
         ]);
+        
         $refreshToken = \conquer\oauth2\models\RefreshToken::createRefreshToken([
-                'client_id' => $this->client_id,
-                'user_id' => \Yii::$app->user->id,
-                'expires' => $this->refreshTokenLifetime + time(),
-                'scope' => $this->scope,
+            'client_id' => $this->client_id,
+            'user_id' => \Yii::$app->user->id,
+            'expires' => $this->refreshTokenLifetime + time(),
+            'scope' => $this->scope,
         ]);
+        
+        $fragment = [
+            'access_token' => $accessToken->access_token,
+            'expires_in' => $this->accessTokenLifetime,
+            'token_type' => self::TOKEN_TYPE,
+            'scope' => $this->scope,
+            'refresh_token' => $refreshToken->refresh_token,
+        ];
+                
+        if (!empty($this->state))
+            $fragment['state'] = $this->state;
+        
         return [
-            'fragment'=>[
-                "access_token" => $accessToken->access_token,
-                "expires_in" => $this->accessTokenLifetime,
-                "token_type" => $this->tokeType,
-                "scope" => $this->scope,
-                'refresh_token' => $refreshToken->refresh_token,
-            ],
+            'fragment'=>$fragment,
         ];
     }
     
